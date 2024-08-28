2249373c56f6e965b09a91a58ba55c60a5c8831b
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-08-28 19:38:55 +0900

    CardListをコンポーネント化した

---
+++ app/components/BBSCardList.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+
+const BBSCardList = () => {
+  return (
+    <div className="grid lg:grid-cols-3 px-4 py-4 gap-4">
+      <Card>
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum neque
+          fuga dolorum natus? Omnis aspernatur expedita sint, debitis est magnam
+          id ratione totam tempora tenetur velit inventore ea possimus minus.
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Link href={"/bbs-posts-1"} className="text-blue-500">
+            Read More
+          </Link>
+        </CardFooter>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum neque
+          fuga dolorum natus? Omnis aspernatur expedita sint, debitis est magnam
+          id ratione totam tempora tenetur velit inventore ea possimus minus.
+        </CardContent>
+        <CardFooter className="flex justify-between"></CardFooter>
+        <Link href={"/bbs-posts-1"} className="text-blue-500">
+          Read More
+        </Link>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>
+            Deploy your new project in one-click.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum neque
+          fuga dolorum natus? Omnis aspernatur expedita sint, debitis est magnam
+          id ratione totam tempora tenetur velit inventore ea possimus minus.
+        </CardContent>
+        <CardFooter className="flex justify-between"></CardFooter>
+        <Link href={"/bbs-posts-1"} className="text-blue-500">
+          Read More
+        </Link>
+      </Card>
+    </div>
+  );
+};
+
+export default BBSCardList;
