532ec4d8528ca0780befa6a6ec52e256040d2c0e
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-08-29 12:12:00 +0900

    データをページに反映させた

--- app/components/BBSCard.tsx
+++ app/components/BBSCard.tsx
@@ -8,21 +8,23 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
+import { BBSData } from "../types/types";
 
-const BBSCard = () => {
+interface BBSDataProps {
+  bbsData: BBSData;
+}
+
+const BBSCard = ({ bbsData }: BBSDataProps) => {
+  const { title, content, createdAt, username, id } = bbsData;
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{username}</CardDescription>
       </CardHeader>
-      <CardContent>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum neque
-        fuga dolorum natus? Omnis aspernatur expedita sint, debitis est magnam
-        id ratione totam tempora tenetur velit inventore ea possimus minus.
-      </CardContent>
+      <CardContent>{content}</CardContent>
       <CardFooter className="flex justify-between">
-        <Link href={"/bbs-posts-1"} className="text-blue-500">
+        <Link href={`/bbs-posts/${id}`} className="text-blue-500">
           Read More
         </Link>
       </CardFooter>
--- app/types/types.ts
+++ app/types/types.ts
@@ -1,4 +1,5 @@
 export interface BBSData {
+  id: number;
   username: string;
   title: string;
   content: String;
